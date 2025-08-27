6ddc22306806bc7718ff1d3755ba9493962c1244
Sachin Yadav <sachin.yadav@tatvasoft.com>
2025-08-27 14:09:35 +0530

    Added Service Worker

--- src/main.tsx
+++ src/main.tsx
@@ -13,6 +13,23 @@ registerSW({
   },
 });
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("./firebase-messaging-sw.js")
+      .then((registration) => {
+        console.log("Service Worker registered: ", registration);
+      })
+      .catch((error) => {
+        console.log("Service Worker registration failed: ", error);
+      });
+  });
+}
+
+navigator.serviceWorker.ready.then(function (registration) {
+  console.log("Service Worker is ready");
+});
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
